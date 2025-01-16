dd70fb9bf21e4fca19f5caa9f2c348b7b5247dd5
Fran García <fgarciadevelop@gmail.com>
2025-01-16 09:34:34 +0100

    f

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -5,15 +5,14 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatTableModule } from '@angular/material/table';
 import { MatTabsModule } from '@angular/material/tabs';
-import { CandidataData, LabelsFormulario, TiposCampos } from '../../model/candidata-data.model';
-import { CensoService } from '../../services/censo.service';
-import { Asociacion, ResponseAsociaciones } from '../../services/external-api/external-api';
-import { FirebaseStorageService } from '../../services/storage.service';
+import { CandidataData, LabelsFormulario } from '../../model/candidata-data.model';
+import { Asociacion } from '../../services/external-api/external-api';
 import { DialogOverviewComponent } from '../dialog-overview/dialog-overview.component';
 
 import { FfsjAlertService } from 'ffsj-web-components';
 import { saveAs } from 'file-saver';
 import * as XLSX from 'xlsx';
+import { CandidataService } from '../../services/candidatas.service';
 
 export interface InfoShowTable {
   id: string;
@@ -63,10 +62,9 @@ export class AdminComponent implements OnInit {
   selectedTab: string = 'adultas';
 
   constructor(
-    private firebaseStorageService: FirebaseStorageService,
-    private censoService: CensoService,
     public dialog: MatDialog,
-    private ffsjAlertService: FfsjAlertService
+    private ffsjAlertService: FfsjAlertService,
+    private candidataService: CandidataService
   ) {
 
   }
@@ -76,139 +74,22 @@ export class AdminComponent implements OnInit {
     this.loadData();
   }
 
-  async loadAsociaciones(): Promise<void> {
-    return new Promise((resolve, reject) => {
-      this.censoService.asociacionesGet().subscribe({
-        next: (response: ResponseAsociaciones) => {
-          if (response.status?.status === 200) {
-            this.asociaciones = response.asociaciones || [];
-            resolve();
-          } else {
-            reject('Error en la respuesta del servidor');
-          }
-        },
-        error: (err) => reject(err)
-      });
-    });
-  }
-
-  async loadFromBD(collection: string): Promise<CandidataData[]> {
-    let arrayData: CandidataData[] = [];
-    try {
-      const data = await this.firebaseStorageService.getCollection(collection);
-      data.forEach(dataBD => {
-        let candidata: CandidataData = {
-          id: { value: dataBD['id'], required: true },
-          informacionPersonal: {
-            dni: { value: dataBD['dni'], required: true },
-            nombre: { value: dataBD['nombre'], required: true },
-            fechaNacimiento: { value: dataBD['fechaNacimiento'], required: true },
-            ciudad: { value: dataBD['ciudad'], required: true },
-            email: { value: dataBD['email'], required: true },
-            telefono: { value: dataBD['telefono'], required: true },
-            edad: { value: dataBD['edad'], required: true },
-            tipoCandidata: { value: dataBD['tipoCandidata'], required: true }
-          },
-          vidaEnFogueres: {
-            asociacion: { value: dataBD['asociacion'], required: true },
-            anyosFiesta: { value: dataBD['anyosFiesta'], required: true },
-            curriculum: { value: dataBD['curriculum'], required: true }
-          },
-          academico: {
-            formacion: { value: dataBD['formacion'], required: true },
-            situacionLaboral: { value: dataBD['situacionLaboral'], required: false },
-            observaciones: { value: dataBD['observaciones'], required: false },
-            aficiones: { value: dataBD['aficiones'], required: false }
-          },
-          documentacion: {
-            autorizacionFoguera: { value: dataBD['autorizacionFoguera'], required: true },
-            compromisoDisponibilidad: { value: dataBD['compromisoDisponibilidad'], required: true },
-            derechosAutor: { value: dataBD['derechosAutor'], required: true },
-            dniEscaneado: { value: dataBD['dniEscaneado'], required: true },
-            fotoBelleza: { value: dataBD['fotoBelleza'], required: true },
-            fotoCalle: { value: dataBD['fotoCalle'], required: true }
-          },
-          responsables: {
-            nombreTutor1: { value: dataBD['nombreTutor1'], required: false },
-            nombreTutor2: { value: dataBD['nombreTutor2'], required: false },
-            telefonoTutor1: { value: dataBD['telefonoTutor1'], required: false },
-            telefonoTutor2: { value: dataBD['telefonoTutor2'], required: false }
-          }
-        };
-        arrayData.push(candidata);
-      });
-    } catch (error) {
-      console.error('Error obteniendo datos:', error);
-    }
-    return arrayData;
-  }
-
   async loadData() {
     try {
-      await this.loadAsociaciones();
-
-      const [adultas, infantiles] = await Promise.all([
-        this.loadFromBD('candidatas/2024/adultas'),
-        this.loadFromBD('candidatas/2024/infantiles')
-      ]);
-
-      this.adultas = adultas;
-      this.infantiles = infantiles;
-
-      ({ nuevasColumnasText: this.columnasAdultasText, nuevasColumnas: this.columnasAdultas, infoTabla: this.adultasData } = this.agrupaColumnas('adultas', this.adultas));
-      ({ nuevasColumnasText: this.columnasInfantilesText, nuevasColumnas: this.columnasInfantiles, infoTabla: this.infantilesData } = this.agrupaColumnas('infantiles', this.infantiles));
-
-      this.updateAsociacionValues(this.adultas, this.adultasData);
-      this.updateAsociacionValues(this.infantiles, this.adultasData);
-
-      console.log(this.adultasData, this.infantilesData);
+      const candidatas = await this.candidataService.getCandidatas();
+      this.infantiles = candidatas.infantiles;
+      this.adultas = candidatas.adultas;
+      this.adultasData = candidatas.adultasData;
+      this.infantilesData = candidatas.infantilesData;
+      this.columnasAdultas = candidatas.columnasAdultas;
+      this.columnasInfantiles = candidatas.columnasInfantiles;
+      this.columnasAdultasText = candidatas.columnasAdultasText;
+      this.columnasInfantilesText = candidatas.columnasInfantilesText;
     } catch (error) {
       console.error('Error cargando datos:', error);
     } finally {
       this.loading = false;
     }
-  }
-
-  updateAsociacionValues(data: CandidataData[], adultasData: InfoShowTable[]): void {
-    data.forEach((item, index) => {
-      const correspondingAdulta = adultasData.find(adulta => adulta.id === item.id.value);
-      if (correspondingAdulta) {
-        item.vidaEnFogueres.asociacion.value = correspondingAdulta.foguera;
-      }
-    });
-  }
-
-  agrupaColumnas(tipoCandidata: string, array: CandidataData[]) {
-    let nuevasColumnas = ['id', 'foguera', 'informacionPersonal', 'vidaEnFogueres', 'academico', 'documentacion'];
-    let nuevasColumnasText = ['Id', 'Foguera', 'Información Personal', 'Vida en Fogueres', 'Académico', 'Documentación'];
-    if (tipoCandidata === 'infantiles') {
-      nuevasColumnas.push('responsables');
-      nuevasColumnasText.push('Responsables');
-    }
-    let infoTabla: any[] = [];
-    array.sort((a, b) => a.vidaEnFogueres.asociacion.value.localeCompare(b.vidaEnFogueres.asociacion.value))
-    array.forEach(c => {
-      let info: InfoShowTable = {
-        id: c.id.value,
-        foguera: this.asociaciones.find(asociacion => { return c.vidaEnFogueres.asociacion.value === String(asociacion.id) })?.nombre || 'Sin datos',
-        informacionPersonal: this.checkCampos(c.informacionPersonal) ? 'Completo' : 'Faltan datos',
-        vidaEnFogueres: this.checkCampos(c.vidaEnFogueres) ? 'Completo' : 'Faltan datos',
-        academico: this.checkCampos(c.academico) ? 'Completo' : 'Faltan datos',
-        documentacion: this.checkCampos(c.documentacion) ? 'Completo' : 'Faltan datos',
-        responsables: this.checkCampos(c.responsables) ? 'Completo' : 'Faltan datos'
-      }
-      infoTabla.push(info);
-    })
-    return { nuevasColumnasText, nuevasColumnas, infoTabla };
-  }
-
-  checkCampos(campos: TiposCampos): boolean {
-    return Object.values(campos).every(campo => {
-      if (campo.required) {
-        return campo.value !== null && campo.value !== undefined && String(campo.value).trim() !== '';
-      }
-      return true;
-    });
   }
 
   openDialog(element: any, col?: string, j?: number): void {
